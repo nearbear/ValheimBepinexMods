984d014bc2a2e62fe833c29ef4e35a3b4f1d4e82
chase <s.chubukov@protonmail.com>
2021-03-10 18:28:21 +0300

    small Minimap.UpdateExplore optimization

--- BiggerBoatMapExploreRadius/Patches/Patches.cs
+++ BiggerBoatMapExploreRadius/Patches/Patches.cs
@@ -12,10 +12,12 @@ namespace BiggerBoatMapExploreRadius.Patches
         [HarmonyPatch(typeof(Minimap), nameof(Minimap.UpdateExplore))]
         public static void UpdateExplorePostfix(ref Minimap __instance, float dt, Player player)
         {
+            if (__instance.m_exploreTimer != 0f) return;
+            
             List<Player> playersInRange = new List<Player>();
             Player.GetPlayersInRange(player.transform.position, 20f, playersInRange);
 
-            if (__instance.m_exploreTimer == 0f && playersInRange.Where(p => p.GetControlledShip()).Any())
+            if (playersInRange.Any(p => p.GetControlledShip()))
             {
                 __instance.Explore(player.transform.position, Main.boatExploreRadius.Value);
             }
